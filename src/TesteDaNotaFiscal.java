56e823c6c93ae31b91392b9cdf3172c52dc1e2c6
Claudio Costa <clacosta@gmail.com>
2019-11-07 10:38:51 -0300

    feet: 06 - 04 - Builder para Item da Nota

--- src/TesteDaNotaFiscal.java
+++ src/TesteDaNotaFiscal.java
@@ -3,10 +3,15 @@ public class TesteDaNotaFiscal {
 
 	public static void main(String[] args) {
 		
+		ItemDaNota item1 = new ItemDaNotaBuilder()
+				.comNome("item1")
+				.comValor(200.0)
+				.constroi();
+		
 		NotaFiscalBuilder builder = new NotaFiscalBuilder()
 		.paraEmpresa("CI&T")
 		.comCNPJ("12.345.678/0001-12")
-		.comItem(new ItemDaNota("item 1", 200.0))
+		.comItem(item1)
 		.comItem(new ItemDaNota("item 2", 300.0))
 		.comItem(new ItemDaNota("item 3", 400.0))
 		.comObservacoes("observacoes")
