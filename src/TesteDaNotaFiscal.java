14f721302d0193a8230723a0d1bdcf75811a9baf
Claudio Costa <clacosta@gmail.com>
2019-11-07 09:15:14 -0300

    feet: 06 - 01 - Criação de objetos e o Builder

---
+++ src/TesteDaNotaFiscal.java
@@ -0,0 +1,20 @@
+
+public class TesteDaNotaFiscal {
+
+	public static void main(String[] args) {
+		
+		NotaFiscalBuilder builder = new NotaFiscalBuilder()
+		.paraEmpresa("CI&T")
+		.comCNPJ("12.345.678/0001-12")
+		.comItem(new ItemDaNota("item 1", 200.0))
+		.comItem(new ItemDaNota("item 2", 300.0))
+		.comItem(new ItemDaNota("item 3", 400.0))
+		.comObservacoes("observacoes")
+		.naDataAtual();
+		
+		NotaFiscal nf = builder.constroi();
+		
+		System.out.println(nf.getValorBruto());
+	}
+	
+}
